ce9b85c9396c34fd8a259aa21638c592b95aa88e
EmyCodez <130977482+EmyCodez@users.noreply.github.com>
2025-06-10 14:48:03 +0400

    ex00 and ex01 complete
    both contains main2.cpp . Need to remove before submission

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -3,14 +3,14 @@
 
 #include <cstddef>
 
-// iter for non-const arrays and functions taking T&
+// iter for non-const arrays and functions
 template <typename T>
-void iter(T* array, size_t length, void (*func)(T&)) {
+void iter(T* array, size_t length, void (*func)(T &)) {
     for (size_t i = 0; i < length; ++i)
         func(array[i]);
 }
 
-// iter for const arrays and functions taking T const&
+// iter for const arrays and functions taking
 template <typename T>
 void iter(T const* array, size_t length, void (*func)(T const&)) {
     for (size_t i = 0; i < length; ++i)
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,34 +2,6 @@
 #include <string>
 #include "iter.hpp"
 
-// ------------------------------------------------------------------------ //
-// class Awesome
-// {
-//     public:
-//     Awesome(void): _n(42) {return;}
-//     int get(void) const {return this->_n;}
-    
-//     private:
-//     int _n;
-// };
-
-// std::ostream &operator << (std::ostream &o, Awesome const &rhs) {o << rhs.get(); return o;}
-
-// template<typename T>
-// void print(T const &x) {std::cout << x<< std::endl; return; }
-
-// int main(){
-//     int tab[] ={0,1,2,3,4};
-//     Awesome tab2[5];
-
-//     iter(tab, 5, print);
-//     iter(tab2, 5, print);
-
-//     return 0;
-// }
-
-//---------------------------------------------------------------------------------
-
 // Print function (const reference)
 template <typename T>
 void print(T const& value) {
@@ -67,7 +39,7 @@ int main() {
 
     std::cout << "\n=== String array ===" << std::endl;
     std::string strArray[] = {"apple", "banana", "cherry"};
-    iter(strArray, 3, print); // Only print, no increment for strings
+    iter(strArray, 3, print);
 
     return 0;
 }
